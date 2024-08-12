de3b259567128f8fdd1d272990918a1eb64704d4
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-12 19:27:45 -0300

    Main carousel image animation with framer added

--- src/components/section/VideoCarousel.tsx
+++ src/components/section/VideoCarousel.tsx
@@ -1,30 +1,63 @@
+import { useMemo, useRef } from "react"
 import { Movie, movies, randomMoviesSet1, randomMoviesSet2 } from "../movies"
+import { useScroll, useTransform, motion } from "framer-motion"
+import { useWindowSize } from "react-use"
 
 const VideoCarousel = () => {
+	const { width, height } = useWindowSize()
+	const carouselWeapperRef = useRef<HTMLDivElement>(null)
+
+	const { scrollYProgress } = useScroll({
+		target: carouselWeapperRef,
+		offset: ["start start", "end start"],
+	})
+
+	const maximumScale = useMemo(() => {
+		const windowYRatio = height / width
+		const xScale = 1.66667
+		const yScale = xScale * (16 / 9) * windowYRatio
+
+		return Math.max(xScale, yScale)
+	}, [width, height])
+
+	const scale = useTransform(
+		scrollYProgress,
+		[0.3, 0.5, 0.66],
+		[maximumScale * 1.1, maximumScale, 1]
+	)
+
 	return (
 		<div className="bg-background pb-8">
-			<div className="overflow-clip">
-				<div className="flex gap-5 mb-5 left-1/2 -translate-x-1/2">
-					<div className="shrink-0 overflow-clip w-[60vh] aspect-video rounded-2xl">
-						<img
-							src={movies[0].poster}
-							className="w-full h-full object-cover"
-							alt={movies[0].name}
-						/>
-					</div>
-					<div className="shrink-0 overflow-clip w-[60vh] aspect-video rounded-2xl">
-						<img
-							src={movies[1].poster}
-							className="w-full h-full object-cover"
-							alt={movies[1].name}
-						/>
-					</div>
-					<div className="shrink-0 overflow-clip w-[60vh] aspect-video rounded-2xl">
-						<img
-							src={movies[2].poster}
-							className="w-full h-full object-cover"
-							alt={movies[2].name}
-						/>
+			<div
+				ref={carouselWeapperRef}
+				className="mt-[-100vh] overflow-clip h-[300vh]"
+			>
+				<div className="h-screen sticky top-0 flex items-center">
+					<div className="relative flex gap-5 mb-5 left-1/2 -translate-x-1/2">
+						<div className="shrink-0 overflow-clip w-[60vh] aspect-video rounded-2xl">
+							<img
+								src={movies[0].poster}
+								className="w-full h-full object-cover"
+								alt={movies[0].name}
+							/>
+						</div>
+						<motion.div
+							style={{ scale }}
+							className="shrink-0 overflow-clip w-[60vh] aspect-video rounded-2xl"
+						>
+							<img
+								src={movies[1].poster}
+								className="w-full h-full object-cover"
+								alt={movies[1].name}
+							/>
+						</motion.div>
+						<div className="shrink-0 overflow-clip w-[60vh] aspect-video rounded-2xl">
+							<img
+								src={movies[2].poster}
+								className="w-full h-full object-cover"
+								alt={movies[2].name}
+							/>
+						</div>
 					</div>
 				</div>
 			</div>
